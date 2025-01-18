fca854dc862d6c387ccce5b822966f0ce6b4cb2b
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-18 16:34:50 +0530

    fetchDogImage logic added

--- lib/screens/DogScreen.dart
+++ lib/screens/DogScreen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../models/Dog.dart';
+import '../utils/Constants.dart';
+
 
 class DogScreen extends StatefulWidget {
 
@@ -12,6 +19,32 @@ class DogScreen extends StatefulWidget {
 
 class _DogScreenState extends State<DogScreen> {
 
+  Dog? dog;
+
+  Future<void> fetchDogImage() async {
+
+    final response = await http.get(Uri.parse(Constants.BASE_URL + Constants.RANDOM_DOG_IMAGE));
+
+    if (response.statusCode == 200) {
+      final Map<String, dynamic> data = json.decode(response.body);
+
+      setState(() {
+        dog = Dog.fromJson(data);
+      });
+    }
+    else
+    {
+      throw Exception("Failed to load image");
+    }
+
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDogImage();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,7 +52,14 @@ class _DogScreenState extends State<DogScreen> {
         title: Text(widget.title)
       ),
       body: Center(
-        child: Text(widget.title),
+        child: dog != null
+            ? Image.network(
+          dog!.message,
+          width: 300,
+          height: 300,
+          fit: BoxFit.cover,
+        )
+            : CircularProgressIndicator(),
       ),
     );
   }
