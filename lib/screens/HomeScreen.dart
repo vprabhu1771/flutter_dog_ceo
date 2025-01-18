5bb253268d5d5685ebdd8be1e3a8fb9969ec697c
vprabhu1771 <vprabhu1771@gmail.com>
2025-01-18 16:44:45 +0530

    fetchDogImage logic added without using Dog Model

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
+
+import '../utils/Constants.dart';
+
+class HomeScreen extends StatefulWidget {
   final String title;
 
   const HomeScreen({super.key, required this.title});
@@ -11,15 +16,78 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  String imageUrl = "";
+
+  bool isLoading = false;
+
+  String errorMessage = '';
+
+  // Function to fetch the dog image
+  Future<void> fetchDogImage() async {
+    setState(() {
+      isLoading = true; // Start loading when network call begins
+      errorMessage = ''; // Reset previous error message
+    });
+
+    try {
+      final response = await http.get(Uri.parse(Constants.BASE_URL + Constants.RANDOM_DOG_IMAGE));
+
+      if (response.statusCode == 200) {
+
+        final Map<String, dynamic> data = json.decode(response.body);
+
+        setState(() {
+          imageUrl = data['message'];
+          isLoading = false; // Stop loading once the image is fetched
+        });
+
+      } else {
+        throw Exception("Failed to load image");
+      }
+    } catch (error) {
+      setState(() {
+        isLoading = false;
+        errorMessage = 'Error: $error'; // Display the error message
+      });
+    }
+  }
+
+  // Function to handle refresh when user pulls or clicks refresh icon
+  Future<void> _handleRefresh() async {
+    await fetchDogImage();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchDogImage(); // Fetch image on initialization
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.refresh),
+            onPressed: _handleRefresh,
+          ),
+        ],
       ),
       body: Center(
-        child: Text(widget.title),
+        child: isLoading
+            ? CircularProgressIndicator() // Show loading spinner when fetching image
+            : imageUrl.isNotEmpty
+            ? Image.network(
+          imageUrl,
+          width: 300,
+          height: 300,
+          fit: BoxFit.cover,
+        )
+            : errorMessage.isNotEmpty
+            ? Text(errorMessage, style: TextStyle(color: Colors.red)) // Show error message if any
+            : Text("No image found"),
       ),
     );
   }
